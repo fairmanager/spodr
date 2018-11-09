829e645ef768a4dd99417b92e8c3fd3f61487e95
Oliver Salzburg <oliver.salzburg@gmail.com>
2018-11-09 09:51:08 +0100

    [TEST] Fix failing tests

--- test/tests.js
+++ test/tests.js
@@ -3,15 +3,15 @@
 const chai           = require( "chai" );
 const expect         = require( "chai" ).expect;
 const chaiAsPromised = require( "chai-as-promised" );
-const sinon          = require( "sinon" );
 const sinonChai      = require( "sinon-chai" );
 const mockery        = require( "mockery" );
-const fs             = require( "fs" );
 
 chai.use( chaiAsPromised );
 chai.use( sinonChai );
 
 describe( "Repository Sorting", () => {
+	let application = null;
+
 	before( () => {
 		mockery.enable( {
 			useCleanCache : true,
@@ -19,17 +19,18 @@ describe( "Repository Sorting", () => {
 			warnOnUnregistered : false
 		} );
 
-		mockery.registerMock( "fs", {
-			existsSync : () => true
-		} );
+		const realFs = require( "fs" );
+		const mockFs = Object.assign( {}, realFs );
+		mockFs.existsSync = () => true;
+		mockery.registerMock( "fs", mockFs );
 
+		application = require( "../lib/app.js" );
 	} );
 	after( () => {
 		mockery.disable();
 	} );
 	it( "Test basic sorting [1]", () => {
-		const application = require( "../lib/app.js" );
-		const a     = {
+		const a = {
 			name : "a",
 			sortAbove : undefined,
 			sortBelow : "b"
@@ -56,8 +57,7 @@ describe( "Repository Sorting", () => {
 		return expect( application.prepareRepositories() ).to.eventually.have.ordered.members( [ b, c, a, d ] );
 	} );
 	it( "Test basic sorting [2]", () => {
-		const application = require( "../lib/app.js" );
-		const a     = {
+		const a = {
 			name : "a",
 			sortAbove : undefined,
 			sortBelow : undefined
@@ -84,8 +84,7 @@ describe( "Repository Sorting", () => {
 		return expect( application.prepareRepositories() ).to.eventually.have.ordered.members( [ a, c, b, d ] );
 	} );
 	it( "Test basic sorting [3]", () => {
-		const application = require( "../lib/app.js" );
-		const a     = {
+		const a = {
 			name : "a",
 			sortAbove : undefined,
 			sortBelow : undefined
@@ -112,8 +111,7 @@ describe( "Repository Sorting", () => {
 		return expect( application.prepareRepositories() ).to.eventually.have.ordered.members( [ a, c, d, b ] );
 	} );
 	it( "Test basic sorting [4]", () => {
-		const application = require( "../lib/app.js" );
-		const a     = {
+		const a = {
 			name : "a",
 			sortAbove : undefined,
 			sortBelow : "d"
@@ -140,8 +138,7 @@ describe( "Repository Sorting", () => {
 		return expect( application.prepareRepositories() ).to.eventually.have.ordered.members( [ b, c, d, a ] );
 	} );
 	it( "Test basic sorting [5]", () => {
-		const application = require( "../lib/app.js" );
-		const a     = {
+		const a = {
 			name : "a",
 			sortAbove : undefined,
 			sortBelow : "e"
@@ -173,8 +170,7 @@ describe( "Repository Sorting", () => {
 		return expect( application.prepareRepositories() ).to.eventually.have.ordered.members( [ d, c, b, e, a ] );
 	} );
 	it( "Test basic sorting [6]", () => {
-		const application = require( "../lib/app.js" );
-		const a     = {
+		const a = {
 			name : "a",
 			sortAbove : undefined,
 			sortBelow : undefined
@@ -201,8 +197,7 @@ describe( "Repository Sorting", () => {
 		return expect( application.prepareRepositories() ).to.eventually.have.ordered.members( [ d, a, c, b ] );
 	} );
 	it( "Test basic sorting [7]", () => {
-		const application = require( "../lib/app.js" );
-		const a     = {
+		const a = {
 			name : "a",
 			sortAbove : undefined,
 			sortBelow : undefined
@@ -229,8 +224,7 @@ describe( "Repository Sorting", () => {
 		return expect( application.prepareRepositories() ).to.eventually.have.ordered.members( [ c, a, b, d ] );
 	} );
 	it( "Test basic sorting [8]", () => {
-		const application = require( "../lib/app.js" );
-		const a     = {
+		const a = {
 			name : "a",
 			sortAbove : undefined,
 			sortBelow : undefined
@@ -257,8 +251,7 @@ describe( "Repository Sorting", () => {
 		return expect( application.prepareRepositories() ).to.eventually.have.ordered.members( [ a, d, c, b ] );
 	} );
 	it( "Test basic sorting [9]", () => {
-		const application = require( "../lib/app.js" );
-		const a     = {
+		const a = {
 			name : "a",
 			sortAbove : undefined,
 			sortBelow : "d"
@@ -285,8 +278,7 @@ describe( "Repository Sorting", () => {
 		return expect( application.prepareRepositories() ).to.eventually.have.ordered.members( [ c, b, d, a ] );
 	} );
 	it( "Sorting 2 Repositories with different settings [1]", () => {
-		const application = require( "../lib/app.js" );
-		const a     = {
+		const a = {
 			name : "a",
 			sortAbove : undefined,
 			sortBelow : undefined
@@ -303,8 +295,7 @@ describe( "Repository Sorting", () => {
 		return expect( application.prepareRepositories() ).to.eventually.have.ordered.members( [ a, b ] );
 	} );
 	it( "Sorting 2 Repositories with different settings [2]", () => {
-		const application = require( "../lib/app.js" );
-		const a     = {
+		const a = {
 			name : "a",
 			sortAbove : undefined,
 			sortBelow : "b"
@@ -321,8 +312,7 @@ describe( "Repository Sorting", () => {
 		return expect( application.prepareRepositories() ).to.eventually.have.ordered.members( [ b, a ] );
 	} );
 	it( "Sorting 2 Repositories with different settings [3]", () => {
-		const application = require( "../lib/app.js" );
-		const a     = {
+		const a = {
 			name : "a",
 			sortAbove : undefined,
 			sortBelow : undefined
@@ -339,8 +329,7 @@ describe( "Repository Sorting", () => {
 		return expect( application.prepareRepositories() ).to.eventually.have.ordered.members( [ b, a ] );
 	} );
 	it( "Sorting 2 Repositories with different settings [4] and expecting an error", () => {
-		const application = require( "../lib/app.js" );
-		const a     = {
+		const a = {
 			name : "a",
 			sortAbove : "b",
 			sortBelow : undefined
@@ -357,8 +346,7 @@ describe( "Repository Sorting", () => {
 		expect( application.prepareRepositories ).to.throw();
 	} );
 	it( "Sorting 2 Repositories with different settings [5] and expecting an error", () => {
-		const application = require( "../lib/app.js" );
-		const a     = {
+		const a = {
 			name : "a",
 			sortAbove : undefined,
 			sortBelow : "b"
@@ -375,8 +363,7 @@ describe( "Repository Sorting", () => {
 		expect( application.prepareRepositories ).to.throw();
 	} );
 	it( "SortAbove itself should throw an error", () => {
-		const application = require( "../lib/app.js" );
-		const a     = {
+		const a = {
 			name : "a",
 			sortAbove : "a",
 			sortBelow : undefined
@@ -393,8 +380,7 @@ describe( "Repository Sorting", () => {
 		expect( application.prepareRepositories ).to.throw();
 	} );
 	it( "SortBelow itself should throw an error", () => {
-		const application = require( "../lib/app.js" );
-		const a     = {
+		const a = {
 			name : "a",
 			sortAbove : "a",
 			sortBelow : undefined
@@ -411,8 +397,7 @@ describe( "Repository Sorting", () => {
 		expect( application.prepareRepositories ).to.throw();
 	} );
 	it( "Using both sortAbove and sortBelow at the same repository should throw an error", () => {
-		const application = require( "../lib/app.js" );
-		const a     = {
+		const a = {
 			name : "a",
 			sortAbove : "a",
 			sortBelow : "b"
@@ -429,8 +414,7 @@ describe( "Repository Sorting", () => {
 		expect( application.prepareRepositories ).to.throw();
 	} );
 	it( "Using sortAbove/sortBelow with an unknown repository name should throw an error", () => {
-		const application = require( "../lib/app.js" );
-		const a     = {
+		const a = {
 			name : "a",
 			sortAbove : "c",
 			sortBelow : undefined
@@ -447,8 +431,7 @@ describe( "Repository Sorting", () => {
 		expect( application.prepareRepositories ).to.throw();
 	} );
 	it( "Setting that would sort indefinitely should throw an error", () => {
-		const application = require( "../lib/app.js" );
-		const a     = {
+		const a = {
 			name : "a",
 			sortAbove : undefined,
 			sortBelow : "b"
@@ -465,8 +448,7 @@ describe( "Repository Sorting", () => {
 		expect( application.prepareRepositories ).to.throw();
 	} );
 	it( "Defining sortAbove/sortBelow with a non-string value should throw an error", () => {
-		const application = require( "../lib/app.js" );
-		const a     = {
+		const a = {
 			name : "a",
 			sortAbove : undefined,
 			sortBelow : undefined
